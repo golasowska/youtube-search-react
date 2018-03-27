e49dee4b5e62d44dfca9e06265c74dc711031a69
golasowska <annagolasowska@gmail.com>
2018-03-27 15:20:04 +0200

    displaying detail

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyAJAnPxx3dYtZ1D7cT1_FG1JIpYCCZpWl0';
 
@@ -11,11 +12,13 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      videos: []
+      videos: [],
+        selectedVideo: null
     };
       YTSearch({ key: API_KEY, term: 'cat' }, (data) => {
           this.setState({
-              videos: data
+              videos: data,
+              selectedVideo: data[0]
           });
       });
 
@@ -24,6 +27,7 @@ export default class App extends React.Component {
     return (
         <div>
             <SearchBar />
+            <VideoDetail video={this.state.selectedVideo} />
             <VideoList videos={this.state.videos} />
         </div>
         )
