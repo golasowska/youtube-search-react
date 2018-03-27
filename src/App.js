91a8144b6d9968a18b36a6c91c87b752a20daa95
golasowska <annagolasowska@gmail.com>
2018-03-27 14:45:22 +0200

    added new components, connected to API, displaying list of videos

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import YTSearch from 'youtube-api-search';
+
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+
+const API_KEY = 'AIzaSyAJAnPxx3dYtZ1D7cT1_FG1JIpYCCZpWl0';
+
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: []
+    };
+      YTSearch({ key: API_KEY, term: 'cat' }, (data) => {
+          this.setState({
+              videos: data
+          });
+      });
+
+  }
   render() {
-    return <div className="">Siemanko!!</div>;
+    return (
+        <div>
+            <SearchBar />
+            <VideoList videos={this.state.videos} />
+        </div>
+        )
   }
 }
